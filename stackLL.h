3aca64aecad4d30126b51b97394050f15e7b1e8b
Brianna Garcia <bg95308@gmail.com>
2025-09-19 00:28:20 -0500

    almostDoneWithStack

--- stackLL.h
+++ stackLL.h
@@ -1,55 +1,95 @@
-class stackLL
-{
+class stackLL {
 private:
-	class node
-	{
+	class node {
 	public:
-		//stores data;
-		int data;
-		//Node* next;
+		int data; 
+		node* next;
+
+		//constructor to initialize nodes with data and a next
+		node(int value) {
+			data = value;
+			next = nullptr;
+		}
 	};
-	node* next;
+	
 	node* top;
 
 public:
 	
 	//constructor
-	//Run-Time:
+	//Run-Time: O(1)
 	stackLL() {
-		//maybe?
-		int data = 0; //i think this is already done in the private class
 		top = nullptr;
-		node* head = top;
-
 	}
 
 	//Take care of memory leaks...(this is the deconstructor)
 	//Run-Time: 
 	~stackLL() {
-		//any temp nodes that are made are to be deleted
-
+		//we make the current node the top node to begin with
+		node* current = top;
+		//when our stack isn't empty
+		while (current != nullptr) {
+			//we create a temporary node 
+			node* temp = current;
+			delete current;
+			current = temp -> next;
+		}
 
 	}
 
 	//return true if empty, false if not
 	//Run-Time: 
 	bool empty() {
-		//check if top node exists, if it does return return false
-		if (!top) {
+		//check if top node exists, if it does return false
+		if (top == nullptr) {
 			return true;
+		}
+		else {
+			return false;
 		}
 	}
 
 	//add item to top of stack
 	//Run-Time:
 	void push(int x) {
-		//add the node after the top node, you can do this recursively. 
+		//make a new node 
+		node* Node = new node(x);
+
+		//check if stack is empty
+		if (top == nullptr) {
+			//set the top to new node
+			top = Node;
+		}
+		else {
+			//link the new node to the current top node
+			Node->next = top;
+
+			//update the top to point to the new node
+			top = Node;
+
+		}
 
 	}
 
 	//remove and return top item from stack
 	//Run-Time:
 	int pop() {
+		//check if stack is empty
+		if (top == nullptr) {
+			return -1; 
+		}
+		else {
+			//saving the current top
+			node* temp = top;
+			//and its data
+			int value = temp->data;
+			//moving top to the next node
+			top = temp->next;
+			//free the old top node
+			delete temp;
+			//return the data
+			return value;
+		}
 
 	}
 
@@ -58,7 +98,7 @@ public:
 	//(and before the i+1 item).
 	//Run-Time:
 	void insertAt(int x, int i) {
-
+		
 	}
 
 };
